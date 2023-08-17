76c7ec931de1ce4b565c80ade977a355b6654971
thlimppp <142520066+thlimppp@users.noreply.github.com>
2023-08-18 00:20:26 +0700

    Add files via upload

---
+++ Exercise4_Theekasupphat_L.py
@@ -0,0 +1,12 @@
+'''Variables come from the first Letter of each word'''
+
+FE = float(input("Input Your Foundation English Score: "))
+GB = float(input("Input Your General Business Score: "))
+ICS = float(input("Input Your Introduction to Computer Systems Score: "))
+CP = float(input("Input Your Computer Programming Score: "))
+
+print("---Your Score---","\n",
+      "General Business: " + str(FE),"\n",
+      "Introduction to Computer Systems: " + str(GB),"\n",
+      "Introduction to Computer Systems: " + str(ICS),"\n",
+      "Computer Programming: " + str(CP),"\n")
