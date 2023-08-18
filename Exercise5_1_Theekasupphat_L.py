55351cbbf1eadaf553442021a9d702d2bb424b3e
thlimppp <142520066+thlimppp@users.noreply.github.com>
2023-08-18 16:54:21 +0700

    Add files via upload

---
+++ Exercise5_1_Theekasupphat_L.py
@@ -0,0 +1,6 @@
+x = float(input("Input any value: "))
+y = float(input("Input any value: "))
+print(x,"+",y,"=", x+y,"\n",
+     x,"-",y,"=", x-y,"\n",
+     x,"*",y,"=", x*y,"\n",
+     x,"/",y,"=", x/y,"\n")
