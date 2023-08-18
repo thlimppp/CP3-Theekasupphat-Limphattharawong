7f706bfb856ae69c28866c7deef8ee4773bfb8d7
thlimppp <142520066+thlimppp@users.noreply.github.com>
2023-08-18 16:54:41 +0700

    Add files via upload

---
+++ Exercise5_2_Theekasupphat_L.py
@@ -0,0 +1,6 @@
+print("--------------Velocity Calculation--------------")
+speed = float(input("Input Speed (Please enter in km): "))
+time = float(input("Input Time (Please enter in hour): "))
+velocity = speed/time
+print("------------------------------------------------")
+print("Velocity : ", velocity, "km/hr")
